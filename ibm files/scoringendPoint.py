c94fa211a658d70c054651a3c11fef5008958c42
Kashish Dharmani <81479525+kashishd12@users.noreply.github.com>
2022-06-15 23:20:41 +0530

    Add files via upload

---
+++ ibm files/scoringendPoint.py
@@ -0,0 +1,20 @@
+import requests
+
+# NOTE: you must manually set API_KEY below using information retrieved from your IBM Cloud account.
+API_KEY = "6SkEc8aBbVddGYdn5ex_Td_kRThQzmK47HBCqr8GRoiH"
+token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey":
+ API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
+mltoken = token_response.json()["access_token"]
+
+header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
+
+# NOTE: manually define and pass the array(s) of values to be scored in the next line
+payload_scoring = {"input_data": [{"fields": [["f0","f1","f2","f3"]], "values":[[0,0,0,0]] }]}
+
+response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/ccbd1726-ec6f-4637-aa66-ff81bd52062c/predictions?version=2022-06-03', json=payload_scoring,
+ headers={'Authorization': 'Bearer ' + mltoken})
+print("Scoring response")
+#print(response_scoring.json())
+pred=response_scoring.json()
+output=pred['predictions'][0]['values'][0][0]
+print(output)
